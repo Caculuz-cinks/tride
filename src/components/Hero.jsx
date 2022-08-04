21589d99caa5baf707290be2ad7dfbb7e3ea0eb3
Oluwaniphise <oduyaleenoch@gmail.com>
2022-08-04 17:03:23 +0100

    added hero bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import hero1 from '../assets/hero1.png'
 import hero2 from '../assets/hero2.png'
+import bg from '../assets/Hero_circle.svg'
 
 export const Hero = () => {
   return (
-    <section className='container py-[6rem] mx-auto mt-[40px]'>
+    <>
+    <section className='relative'>
+      <div className='absolute right-0 top-[0px]'>
+        <img src={bg} alt="" />
+      </div>
+
+    </section>
+    <section className='container mb:[4rem] font-semibold font-[400] py-[2rem] md:py-[2rem] mx-auto mt-[20px]'>
       <div className='flex flex-row gap-x-[100px]'>
         <div className='max-w-[500px]'>
-        <h1 className='text-white font-semibold font-[400] text-[60px] text-left'>
+        <h1 className='text-white text-[40px]  md:text-[60px] text-left'>
         Banking solution 
          for <span className='text-yellow'>churches</span>
         </h1>
@@ -37,5 +45,7 @@ export const Hero = () => {
       </div>
 
     </section>
+    </>
+
   )
 }
